f70b2345efaf03ba7c04e3acc4ee2fe87edc4f10
MuhammadAnas <khananas1291@gmail.com>
2026-02-27 22:33:59 -0700

    Testing delete city

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -12,4 +12,14 @@ public class CustomListTest {
         // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
     }
+    @Test
+    void testDeleteCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+
+        list.addCity(calgary);
+        list.deleteCity(calgary);
+
+        assertFalse(list.hasCity(calgary));
+    }
 }
